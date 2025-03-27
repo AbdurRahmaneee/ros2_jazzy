2315d793d83057f3dd214a7d808355dfb6bcfb14
Abdur Rahman <ar.abdurrahmaneee@gmail.com>
2025-03-27 06:00:03 +0100

    Initial commit

---
+++ build/my_robot_interfaces/my_robot_interfaces__py/CMakeFiles/my_robot_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_c.c"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/_my_robot_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/__init__.py"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_hardware_status.py"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/msg/_hardware_status_s.c"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/__init__.py"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/_compute_rectangle_area.py"
+  "/home/abdur-rahman/ros2_ws/build/my_robot_interfaces/rosidl_generator_py/my_robot_interfaces/srv/_compute_rectangle_area_s.c"
+  "CMakeFiles/my_robot_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/my_robot_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
